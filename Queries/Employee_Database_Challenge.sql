3994760665a47000daf53a202281f48b1be51b02
Janelle Anderson <janelleanderson@Janelles-MacBook-Air-4.local>
2021-06-05 15:39:12 -0400

    addin  deliverable 1

---
+++ Queries/Employee_Database_Challenge.sql
@@ -0,0 +1,35 @@
+--Create list of retirement titles
+SELECT DISTINCT e.emp_no,
+				e.first_name,
+				e.last_name,
+				t.title,
+				t.from_date,
+				t.to_date
+into retirement_titles
+from employees as e
+inner join titles as t
+on (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+--create list of unique titles
+SELECT DISTINCT On (e.emp_no) e.first_name,
+				e.last_name,
+				t.title,
+				t.from_date,
+				t.to_date
+into unique_titles
+from employees as e
+inner join titles as t
+on (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no, t.to_date Desc;
+
+--Count number of retirees per title
+SELECT Count(ut.title), ut.title
+into retiring_titles
+from unique_titles as ut
+group by ut.title
+order by count(ut.title) desc
+
